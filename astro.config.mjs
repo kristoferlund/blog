d0d74b45d13c621df435006b530d272157ba9a8d
Kristofer <kristofer@fmckl.se>
2024-07-03 15:14:13 +0200

    refactor: Remove Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://avwrm-5iaaa-aaaal-qdhcq-cai.icp0.io",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    icon(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
